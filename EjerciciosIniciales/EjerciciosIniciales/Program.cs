4eff4489718529f282c40ca1d93d65f0a79bee94
KTAROM <kta_rom@hotmail.com>
2021-08-25 18:49:59 -0300

    Ejercicio 1
    Resoluciòn de ejercicio 1 de la guía

---
+++ EjerciciosIniciales/EjerciciosIniciales/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosIniciales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicios Iniciales 1
+            int num = 1;
+            string imprimir = "";
+            for (num = 1; num <= 465; num++)
+            {
+                imprimir += num;
+                if(num%5==0 && num%3==0)
+                {
+                    imprimir += "FooBar";
+                }
+                else { 
+                    if (num % 3 == 0)
+                    { imprimir += "Foo"; }
+                    else
+                    {
+                        if (num % 5 == 0)
+                            imprimir += "Bar";
+                    }
+                }
+                imprimir += "\n";
+            }
+            Console.WriteLine(imprimir);
+            Console.ReadKey();
+        
+
+
+        }
+    }
+}
