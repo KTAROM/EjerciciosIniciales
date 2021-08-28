7281ef0cbe93a5468d263df78ddfb32d25615174
KTAROM <kta_rom@hotmail.com>
2021-08-27 21:18:03 -0300

    Update Program.cs
    Ejercicio 4

--- EjerciciosIniciales/EjerciciosIniciales/Program.cs
+++ EjerciciosIniciales/EjerciciosIniciales/Program.cs
@@ -11,62 +11,107 @@ namespace EjerciciosIniciales
         static void Main(string[] args)
         {
             //Ejercicios Iniciales 1
-            int num = 1;
-            string imprimir = "";
-           for (num = 1; num <= 465; num++)
+             int num = 1;
+              string imprimir = "";
+              for (num = 1; num <= 465; num++)
+              {
+                  imprimir += num;
+                  if (num % 5 == 0 && num % 3 == 0)
+                  {
+                      imprimir += "FooBar";
+                  }
+                  else
+                  {
+                      if (num % 3 == 0)
+                      { imprimir += "Foo"; }
+                      else
+                      {
+                          if (num % 5 == 0)
+                              imprimir += "Bar";
+                      }
+                  }
+                  imprimir += "\n";
+              }
+              Console.WriteLine(imprimir);
+              Console.ReadKey();
+              //Ejercicios inciales 2
+
+              num = 1;
+              imprimir = "";
+              for (num = 1; num <= 465; num++)
+              {
+                  imprimir += num;
+                  if (num % 3 == 0)
+                  {
+                      imprimir += " es divisible por 3";
+                  }
+
+                  else
+                  {
+                      int prueba = 1;
+                      int div = num - 1;
+
+                      while (prueba > 0 && div > 1)
+                      {
+                          prueba = num % div;
+                          div -= 1;
+
+                      }
+                      if (prueba != 0)
+                      {
+                          imprimir += " es primo";
+                      }
+
+                  }
+                  imprimir += "\n";
+              }
+              Console.WriteLine(imprimir);
+              Console.ReadKey();
+
+              // Ejercicio 3
+
+              Console.WriteLine("Ingrese una palabra");
+              string palabra = Console.ReadLine();
+              int cantlet = palabra.Length;
+              int d = cantlet-1;
+
+              char[] letras = new char[cantlet];
+              for ( int i=0; i<cantlet; i++)
+              {                
+                  letras[i] = palabra[d];
+                  d -= 1;
+
+              }
+
+              string palabra2 = new string(letras);
+              if(palabra==palabra2)
+              { Console.WriteLine("Es palíndromo"); }
+              Console.ReadKey();
+
+            // Ejercicio 4
+
+            Console.WriteLine("Escriba una palabra");
+            string palabra1 = Console.ReadLine();
+            Console.WriteLine("Escriba una palabra");
+            palabra2 = Console.ReadLine();
+            if (palabra1.Length==palabra2.Length)
             {
-                imprimir += num;
-                if (num % 5 == 0 && num % 3 == 0)
+                char[] pal = new char[palabra1.Length];
+                d = palabra1.Length - 1;
+                    for(int i=0; i<palabra1.Length; i++)
                 {
-                    imprimir += "FooBar";
+                    pal[i] = palabra1[d];
+                    d -= 1;
                 }
-                else
+                string pal1 = new string(pal);
+                if (palabra2 == pal1)
                 {
-                    if (num % 3 == 0)
-                    { imprimir += "Foo"; }
-                    else
-                    {
-                        if (num % 5 == 0)
-                            imprimir += "Bar";
-                    }
+                    Console.WriteLine("Son un anagrama");
                 }
-                imprimir += "\n";
+                Console.WriteLine("No son un anagrama");
             }
-            Console.WriteLine(imprimir);
-            Console.ReadKey();
-            //Ejercicios inciales 2
 
-           num = 1;
-            imprimir = "";
-            for (num = 1; num <= 465; num++)
-            {
-                imprimir += num;
-                if (num % 3 == 0)
-                {
-                    imprimir += " es divisible por 3";
-                }
 
-                else
-                {
-                    int prueba = 1;
-                    int div = num-1;
-
-                    while (prueba > 0 && div > 1)
-                    {
-                        prueba = num % div;
-                        div -= 1;
-                        
-                    }
-                    if (prueba != 0)
-                    {
-                        imprimir += " es primo";
-                    }
-
-                }
-                imprimir += "\n";
-            }
-            Console.WriteLine(imprimir);
-            Console.ReadKey();
         }
     }
 }
