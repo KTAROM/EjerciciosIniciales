af989e8a80972a32e985b037364b4de9aee733e2
KTAROM <kta_rom@hotmail.com>
2021-08-29 22:42:09 -0300

    Update Program.cs
    Resolucion hasta ej 8

--- EjerciciosIniciales/EjerciciosIniciales/Program.cs
+++ EjerciciosIniciales/EjerciciosIniciales/Program.cs
@@ -13,112 +13,134 @@ namespace EjerciciosIniciales
             //Ejercicios Iniciales 1
             int num = 1;
             string imprimir = "";
-           /* for (num = 1; num <= 465; num++)
+            /* for (num = 1; num <= 465; num++)
+             {
+                 imprimir += num;
+                 if (num % 5 == 0 && num % 3 == 0)
+                 {
+                     imprimir += "FooBar";
+                 }
+                 else
+                 {
+                     if (num % 3 == 0)
+                     { imprimir += "Foo"; }
+                     else
+                     {
+                         if (num % 5 == 0)
+                             imprimir += "Bar";
+                     }
+                 }
+                 imprimir += "\n";
+             }
+             Console.WriteLine(imprimir);
+             Console.ReadKey();
+             //Ejercicios inciales 2
+
+             num = 1;
+             imprimir = "";
+             for (num = 1; num <= 465; num++)
+             {
+                 imprimir += num;
+                 if (num % 3 == 0)
+                 {
+                     imprimir += " es divisible por 3";
+                 }
+
+                 else
+                 {
+                     int prueba = 1;
+                     int div = num - 1;
+
+                     while (prueba > 0 && div > 1)
+                     {
+                         prueba = num % div;
+                         div -= 1;
+
+                     }
+                     if (prueba != 0)
+                     {
+                         imprimir += " es primo";
+                     }
+
+                 }
+                 imprimir += "\n";
+             }
+             Console.WriteLine(imprimir);
+             Console.ReadKey();
+
+             // Ejercicio 3
+
+             Console.WriteLine("Ingrese una palabra");
+             string palabra = Console.ReadLine();
+             int cantlet = palabra.Length;
+             int d = cantlet - 1;
+
+             char[] letras = new char[cantlet];
+             for (int i = 0; i < cantlet; i++)
+             {
+                 letras[i] = palabra[d];
+                 d -= 1;
+
+             }
+
+             string palabra2 = new string(letras);
+             if (palabra == palabra2)
+             { Console.WriteLine("Es palíndromo"); }
+             Console.ReadKey();
+
+             // Ejercicio 4
+
+             Console.WriteLine("Escriba una palabra");
+             string palabra1 = Console.ReadLine();
+             Console.WriteLine("Escriba una palabra");
+             palabra2 = Console.ReadLine();
+             if (palabra1.Length == palabra2.Length)
+             {
+
+                 char[] pal = Armarchar(palabra1);
+                 char[] pal2 = Armarchar(palabra2);
+                 palabra1 = Ordenar(palabra1, pal);
+                 palabra2 = Ordenar(palabra2, pal2);
+                 if (palabra1 == palabra2)
+                 {
+                     Console.WriteLine("Son anagramas");
+                 }
+                 else { Console.WriteLine("No son anagramas"); }
+             }
+
+
+             //Ejercicio 5
+             Console.WriteLine("Escriba un número de 5 cifras");
+             string numero = Console.ReadLine();
+            char[] numero1= Armarchar(numero);
+             numero1 = (OrdenarNum(numero1));
+             numero = new string(numero1);
+             int num1 = int.Parse(numero);
+             Console.WriteLine("El número invertido es: "+num1);
+
+            // Ejercicio6
+            Console.WriteLine("Ingrese el primer número");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine((num1 + num2) * (num1 - num2));*/
+
+            // Ejercicio 7
+
+            int[] numero = new int[5];
+            for (int i = 0; i < 5; i++)
             {
-                imprimir += num;
-                if (num % 5 == 0 && num % 3 == 0)
-                {
-                    imprimir += "FooBar";
-                }
-                else
-                {
-                    if (num % 3 == 0)
-                    { imprimir += "Foo"; }
-                    else
-                    {
-                        if (num % 5 == 0)
-                            imprimir += "Bar";
-                    }
-                }
-                imprimir += "\n";
+                Console.WriteLine("Ingrese el número " + (i + 1));
+                numero[i] = int.Parse(Console.ReadLine());
             }
-            Console.WriteLine(imprimir);
-            Console.ReadKey();
-            //Ejercicios inciales 2
-
-            num = 1;
-            imprimir = "";
-            for (num = 1; num <= 465; num++)
-            {
-                imprimir += num;
-                if (num % 3 == 0)
-                {
-                    imprimir += " es divisible por 3";
-                }
-
-                else
-                {
-                    int prueba = 1;
-                    int div = num - 1;
-
-                    while (prueba > 0 && div > 1)
-                    {
-                        prueba = num % div;
-                        div -= 1;
-
-                    }
-                    if (prueba != 0)
-                    {
-                        imprimir += " es primo";
-                    }
-
-                }
-                imprimir += "\n";
-            }
-            Console.WriteLine(imprimir);
+            int maximo = Maximo(numero);
+            int minimo = Minimo(numero);
+            numero = Ordenar2(numero);
+            int medio = numero[2];
+            Console.WriteLine("El número máximo es " + maximo + "\n" + "El número intermedio es " + medio + "\n" + "El número mínimo es " + minimo);
+           
             Console.ReadKey();
 
-            // Ejercicio 3
-
-            Console.WriteLine("Ingrese una palabra");
-            string palabra = Console.ReadLine();
-            int cantlet = palabra.Length;
-            int d = cantlet - 1;
-
-            char[] letras = new char[cantlet];
-            for (int i = 0; i < cantlet; i++)
-            {
-                letras[i] = palabra[d];
-                d -= 1;
-
-            }
-
-            string palabra2 = new string(letras);
-            if (palabra == palabra2)
-            { Console.WriteLine("Es palíndromo"); }
-            Console.ReadKey();
-
-            // Ejercicio 4
-
-            Console.WriteLine("Escriba una palabra");
-            string palabra1 = Console.ReadLine();
-            Console.WriteLine("Escriba una palabra");
-            palabra2 = Console.ReadLine();
-            if (palabra1.Length == palabra2.Length)
-            {
-
-                char[] pal = Armarchar(palabra1);
-                char[] pal2 = Armarchar(palabra2);
-                palabra1 = Ordenar(palabra1, pal);
-                palabra2 = Ordenar(palabra2, pal2);
-                if (palabra1 == palabra2)
-                {
-                    Console.WriteLine("Son anagramas");
-                }
-                else { Console.WriteLine("No son anagramas"); }
-            }*/
-
-
-            //Ejercicio 5
-            Console.WriteLine("Escriba un número de 5 cifras");
-            string numero = Console.ReadLine();
-           char[] numero1= Armarchar(numero);
-            numero1 = (OrdenarNum(numero1));
-            numero = new string(numero1);
-            int num1 = int.Parse(numero);
-            Console.WriteLine("El número invertido es: "+num1);
-
-            Console.ReadKey();
         }
 
         private static char[] Armarchar(string palabra)
@@ -151,12 +173,12 @@ namespace EjerciciosIniciales
             string palordenada = new string(pal);
             return palordenada;
         }
-        
-       public static char[] OrdenarNum(char[]num)
+
+        public static char[] OrdenarNum(char[] num)
         {
             int j = num.Length - 1;
             int i = 0;
-           while (i<j)
+            while (i < j)
             {
                 do
                 {
@@ -166,11 +188,64 @@ namespace EjerciciosIniciales
                     i += 1;
                     j -= 1;
                 } while (i > j);
-            } 
+            }
 
             return num;
         }
-        
+        public static int Maximo(int[] num)
+        {
+
+            int cant = num.Length - 1;
+            int max = num[cant];
+
+            for (int i = 0; i <= cant; i++)
+            {
+                if (num[i] > max)
+                {
+                    max = num[i];
+
+                }
+            }
+            return max;
+            
+
+
+        }
+        public static int Minimo(int[] num)
+        {
+
+            int cant = num.Length - 1;
+            int min = num[cant];
+
+            for (int i = 0; i <= cant; i++)
+            {
+                if (num[i] < min)
+                {
+                    min = num[i];
+
+                }
+            }
+            return min;
+
+
+        }
+        private static int[] Ordenar2(int[] num)
+        {
+            int cant = num.Length;
+            for (int j = 0; j < cant; j++)
+            {
+                for (int i = 0; i < cant - 1; i++)
+                {
+                    if (num[i] > num[i + 1])
+                    {
+                        int aux = num[i];
+                        num[i] = num[i + 1];
+                        num[i + 1] = aux;
+                    }
+                }
+            }            
+            return num;
+        }
     }
 }
 
